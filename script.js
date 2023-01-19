23a942c50220a9763f565b2a2ab8d4f4ee6c6b83
KamilKazmierczak666 <kkazmierczak6@sii.pl>
2023-01-19 16:08:35 +0100

    I dont even know anymore

---
+++ script.js
@@ -0,0 +1,9 @@
+const productsEl = document.querySelector(".products")
+
+function renderProducts() {
+    products.forEach( (product) =>
+        productsEl.innerHTML += ` 
+        
+        `
+    )
+}
